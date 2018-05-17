13099fe1d9780de75829657dcb9b7cbdc2fee4a2
Iain <Iain@DESKTOP-BKE6KM9>
2018-05-16 20:56:18 -0500

    Initial commit of config service.

---
+++ config-service/src/test/java/com/springcloud/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.springcloud.configservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("native")
+public class ConfigServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
